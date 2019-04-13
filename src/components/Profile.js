e465706c7da515cfbefb5e9d4c9cd575f7f493e4
halented <hallie.dunn1@gmail.com>
2019-04-13 16:39:03 -0700

    render routes conditionally

---
+++ src/components/Profile.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Profile extends Component {
+    render() {
+        return (
+            <div>
+                renderin' a profile here
+            </div>
+        );
+    }
+}
+
+export default Profile;
