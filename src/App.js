2c23e63ed496fdafb31cdb6edae6f2e4bd21ecc1
Jessica Poe <jesspoe00@gmail.com>
2019-04-13 22:26:43 -0700

    connected and created login form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import {BrowserRouter as Router, Route, NavLink} from 'react-router-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
 import SignUp from './components/SignUp'
 import Login from './components/Login'
@@ -19,27 +19,28 @@ class App extends Component {
       expenses: 0,
       income: 0,
       selectedPlan: null,
-      numberOfDebts: 1
+      numberOfDebts: 1,
+      user_id: 0
     }
     this.addNewDebt = this.addNewDebt.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-  addNewDebt(ev){
+  addNewDebt(ev) {
     ev.preventDefault()
     console.log("ADDING A NEW DEBT")
-    this.setState({numberOfDebts: this.state.numberOfDebts + 1})
+    this.setState({ numberOfDebts: this.state.numberOfDebts + 1 })
   }
 
-  handleSubmit(ev){
+  handleSubmit(ev) {
     ev.preventDefault()
     let tempArr = []
-    for(let i=0; i < this.state.numberOfDebts; i++){
+    for (let i = 0; i < this.state.numberOfDebts; i++) {
       let obj = {}
-    let typ = document.getElementById(i).children[2].value
-    let amt = document.getElementById(i).children[6].value
-    let int = document.getElementById(i).children[10].value
-    let minpay = document.getElementById(i).children[14].value
+      let typ = document.getElementById(i).children[2].value
+      let amt = document.getElementById(i).children[6].value
+      let int = document.getElementById(i).children[10].value
+      let minpay = document.getElementById(i).children[14].value
       obj['type'] = typ
       obj['amount'] = amt
       obj['interest'] = int
@@ -49,32 +50,54 @@ class App extends Component {
     console.log(tempArr)
   }
 
+  onLogIn = (event, username) => {
+    event.preventDefault(event)
+    fetch(`localhost:3000/users`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        username: username
+      })
+    })
+      .then(json => {
+        if (json.status === 400) {
+          return
+        } else {
+          this.setState({
+            user_id: json.id,
+            userExists: true
+          })
+        }
+      }
+      )
+  }
+
   render() {
     return (
-      <div className="App">
-        WELCOME TO DEBT-DRAGON!
+      <div className="App" >
+        WELCOME TO DEBT- DRAGON!
         <>
           <Router>
             <div>
               {this.state.userExists ?
-              <NavLink to="/profile">Profile</NavLink>
-              :
-              <>
-              <NavLink to="/signup">SignUp</NavLink>
-              <NavLink to="/login">Login</NavLink>
-              </>
+                <NavLink to="/profile">Profile</NavLink>
+                :
+                <>
+                  <NavLink to="/signup">SignUp</NavLink>
+                  <NavLink to="/login">Login</NavLink>
+                </>
               }
               <div>
-              <Route path="/login" component={Login}/>
-              <Route path="/profile" component={Profile}/>
-              <Route path="/signup" render={(props) => (
-                <SignUp {...props} debts={this.state.debts} addNewDebt={this.addNewDebt} handleSubmit={this.handleSubmit} numberOfDebts={this.state.numberOfDebts}/>
-              )} />
+                <Route path="/login" component={Login} />
+                <Route path="/profile" component={Profile} />
+                <Route path="/signup" render={(props) => (
+                  <SignUp {...props} debts={this.state.debts} addNewDebt={this.addNewDebt} handleSubmit={this.handleSubmit} numberOfDebts={this.state.numberOfDebts} />
+                )} />
               </div>
             </div>
           </Router>
         </>
-      </div>
+      </div >
     );
   }
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+  constructor() {
+    super()
+    this.state = {
+      username: ""
+    }
+  }
+
+
+  onChange = () => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+
   render() {
     return (
       <div>
-        LOGIN ;)
+        <form onChange={(event) => this.handleChange(event)} onSubmit={(event) => this.props.onLogIn(event, this.state)}>
+          <label>Username: </label>
+          <input name="username" value={this.state.username} />
+          <input name="submit" value="Submit" />
+        </form>
       </div>
     );
   }
